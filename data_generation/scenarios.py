564ac9ae45087b59d7960112b51209e89b87937d
Christopher Strøm <detoxte@gmail.com>
2023-01-10 16:02:24 +0100

    Move plot function from base scenario and add new printouts
    Signed-off-by: Christopher Strøm <detoxte@gmail.com>

--- data_generation/scenarios.py
+++ data_generation/scenarios.py
@@ -1,82 +1,44 @@
-"""
-This file holds all the manually crafted scenarios, which for the time being
-is one where all targets intersect at a given point, and the other being CV
-objects being birthed and killed stochastically, the former with a possion-
-gaussian birth model, and the other a bernoulli.
-
-To make a scenario follow the basic single-target assumptions, set the probability
-of survival to 1 and the birth intensity to 0
-
-TODO: YAML configuration for scenario
-TODO: Own class for plotting
-TODO: Animate plots
-TODO: Step forward backward with buttons
-"""
+import argparse
 
 import numpy as np
 import matplotlib.pyplot as plt
-from dataclasses import dataclass
-
 
 from constant_velocity_object import CVObject, Measurement
 
-from utility import step_from_time, time_from_step
-
-
-
-
-@dataclass
-class ScenarioConfig:
-    dt: float
-
-    n_starting_targets: int
-    birth_intensity: int
-    mean_birth_pos: np.ndarray
-    var_birth_pos: np.ndarray
-    mean_birth_vel: np.ndarray
-    var_birth_vel: np.ndarray
-
-    probability_survival: float
-    probability_detection: float
-
-    process_noise: float # For the CV model
-    measurement_noise: float # variance for position
-
-    clutter_intensity: float
-
-    sensor_range_max: float # Square with sidelength 2*sensor_range_max
-    sensor_range_min: float
-    sensor_position: np.ndarray = np.array((0, 0))
-
-
+from utility import time_from_step
+from load_config import load_yaml_into_dotdict
 
 
 class BaseScenario:
     """
     A scenario takes some config for its input, and returns a set of measurements
-    for the current timestep, or of multiple timesteps. 
+    for the current timestep, or of multiple timesteps.
 
 
     """
 
-    def __init__(self, config: ScenarioConfig) -> None:
+    def __init__(self, config) -> None:
         self.config = config
         self.alive_targets = []
-        self.k = 0 # current timestep. Maintain inside base?
-        self.id = 0 # maintain an ID counter that is increased for every new target
+        self.dead_targets = []
+        self.k = 0  # current timestep. Maintain inside base?
+        self.id = 0  # maintain an ID counter that is increased for every new target
 
         for _ in range(config.n_starting_targets):
             self.alive_targets.append(self._new_cv_object())
 
-
     def _new_cv_object(self):
         object = CVObject(
-            np.random.multivariate_normal(self.config.mean_birth_pos, self.config.var_birth_pos),
-            np.random.multivariate_normal(self.config.mean_birth_vel, self.config.var_birth_vel),
+            np.random.multivariate_normal(
+                self.config.birth.position.mean, self.config.birth.position.var
+            ),
+            np.random.multivariate_normal(
+                self.config.birth.velocity.mean, self.config.birth.velocity.var
+            ),
             self.id,
-            self.config.process_noise,
+            self.config.noise.process,
             self.config.dt,
-            time_from_step(self.k, self.config.dt)
+            time_from_step(self.k, self.config.dt),
         )
         self.id += 1
 
@@ -85,8 +47,8 @@ class BaseScenario:
     def _is_target_in_range(self, target):
         target_in_sensor_range = True
 
-        lmax = self.config.sensor_range_max
-        lmin = self.config.sensor_range_min
+        lmax = self.config.sensor.range.max
+        lmin = self.config.sensor.range.min
 
         if not lmax > np.abs(target.track[-1, 1]) > lmin:
             target_in_sensor_range = False
@@ -94,68 +56,74 @@ class BaseScenario:
             target_in_sensor_range = False
 
         return target_in_sensor_range
-        
 
     def generate_clutter(self):
-        n_clutter = np.random.poisson(self.config.clutter_intensity)
+        n_clutter = np.random.poisson(self.config.noise.clutter_intensity)
 
         clutter = []
         for n in range(n_clutter):
             # TODO: Account for sensor position
-            pos = np.random.uniform(-self.config.sensor_range_max, self.config.sensor_range_max, (2,))
-            measurement = Measurement(pos, time_from_step(self.k, self.config.dt), is_clutter=True)
+            pos = np.random.uniform(
+                -self.config.sensor.range.max, self.config.sensor.range.max, (2,)
+            )
+            measurement = Measurement(
+                pos, time_from_step(self.k, self.config.dt), is_clutter=True
+            )
             clutter.append(measurement)
         return clutter
 
     def measurement_from_tracks(self):
         """
         Drop measurements with probability 1 - p_d or if target is out of sensor
-        area. 
+        area.
         """
         measurements = []
 
         for target in self.alive_targets:
             p_drop = np.random.uniform()
-            if p_drop > self.config.probability_detection:
+            if p_drop > self.config.probability.detection:
                 continue
-            
+
             if not self._is_target_in_range(target):
                 continue
 
-            position = np.array(target.track[-1, 1:3]) + np.random.normal((0, 0), self.config.measurement_noise*np.ones(2))
+            position = np.array(target.track[-1, 1:3]) + np.random.normal(
+                (0, 0), self.config.noise.measurement * np.ones(2)
+            )
 
-            measurement = Measurement(position, time_from_step(self.k, self.config.dt), is_clutter=False)
+            measurement = Measurement(
+                position, time_from_step(self.k, self.config.dt), is_clutter=False
+            )
             measurements.append(measurement)
 
         return measurements
 
-
-
     def step(self):
-    
+
         # Kill existing targets
         n_alive_targets = len(self.alive_targets)
         for i in range(n_alive_targets):
             p_kill = np.random.uniform()
-            if p_kill > self.config.probability_survival:
-                self.alive_targets.pop(i)
+            if p_kill > self.config.probability.survival:
+                dead_target = self.alive_targets.pop(
+                    np.random.randint(len(self.alive_targets))
+                )
+                self.dead_targets.append(dead_target)
 
         for target in self.alive_targets:
             target.step()
 
         # Spawn new targets according to a PPP birth model
-        n_new_targets = np.random.poisson(self.config.birth_intensity)
+        n_new_targets = np.random.poisson(self.config.birth.intensity)
         for _ in range(n_new_targets):
             self.alive_targets.append(self._new_cv_object())
-            
 
     def run(self, n_timesteps):
 
         hist_measurements = []
 
         for _ in range(n_timesteps):
-            self.step() # Step ground truth targets
-            
+            self.step()  # Step ground truth targets
 
             # Generate true and false measurements and append to final list
             true_measurements = self.measurement_from_tracks()
@@ -166,66 +134,65 @@ class BaseScenario:
 
             self.k += 1
 
+        ground_truths = self.dead_targets + self.alive_targets
 
-        return hist_measurements
-    
+        return hist_measurements, ground_truths
+
     def ground_truths(self):
         raise NotImplementedError
 
 
-    def plot(self, measurements):
-        """
-        TODO: Move to own class
-        """
+def plot(scenario, measurements, ground_truths):
 
-        # Ground truths
-        for target in self.alive_targets:
-            track = target.track
-            x = track[:, 1]
-            y = track[:, 2]
-            plt.scatter(x, y)
+    end_time = time_from_step(scenario.k, scenario.config.dt)
+    min_alpha = 0.5
 
-        # Measurements
-        for measurement in measurements:
-            # measurements at time t
-            for point in measurement:
-                color = "r" if point.is_clutter else "k"                
-                plt.scatter(point.pos[0], point.pos[1], marker="x", color=color)
+    # Ground truths
+    for target in ground_truths:
+        track = target.track
+        x = track[:, 1]
+        y = track[:, 2]
+        alpha = 1 - np.vectorize(max)(min_alpha, 1 - track[:, 5] / end_time)
+        plt.scatter(x, y, alpha=alpha)
 
+    # Measurements
+    for measurements_at_t in measurements:
 
-        plt.show()
+        # opacity based on time
+        for measurement in measurements_at_t:
+            alpha = 1 - max(min_alpha, 1 - measurement.t / end_time)
+            color = "r" if measurement.is_clutter else "k"
+            plt.scatter(
+                measurement.pos[0],
+                measurement.pos[1],
+                marker="x",
+                color=color,
+                alpha=alpha,
+            )
 
+    plt.xlabel("x [m]")
+    plt.ylabel("y [m]")
 
-class ScenarioPlotter:
+    plt.gca().set_aspect("equal")
+    plt.grid()
+    plt.show()
 
-    def __init__(self):
-        pass
-
-    def plot(self):
-        pass
 
 if __name__ == "__main__":
-    # Plot a single scenario
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config")
+    args = parser.parse_args()
 
-    config = ScenarioConfig(
-        n_starting_targets=1,
-        birth_intensity=0,
-        mean_birth_pos=np.array((0, 0)),
-        var_birth_pos=np.eye(2),
-        mean_birth_vel=np.array((1, 1)),
-        var_birth_vel=np.eye(2),
-        probability_survival=1,
-        probability_detection=0.9,
-        process_noise=1,
-        measurement_noise=0.1,
-        clutter_intensity=1,
-        sensor_range_max=10,
-        sensor_range_min=0.1,
-        dt = 0.1,
-    )
-
+    print(f"Using config from: {args.config}")
+    config = load_yaml_into_dotdict(args.config)
 
     scenario = BaseScenario(config)
 
-    measurements = scenario.run(100)
-    scenario.plot(measurements)
+    measurements, ground_truths = scenario.run(50)
+
+    for measurements_at_t in measurements:
+        for measurement in measurements_at_t:
+            print(measurement)
+        print()
+
+    plot(scenario, measurements, ground_truths)
